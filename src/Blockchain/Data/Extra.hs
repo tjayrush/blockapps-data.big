91993df80a0920da78866d2005a584377717b88b
user <user@blockapps.net>
2015-10-06 14:50:56 -0400

    Adding code to store the best block.

--- src/Blockchain/Data/Extra.hs
+++ src/Blockchain/Data/Extra.hs
@@ -4,7 +4,9 @@ module Blockchain.Data.Extra (
      putBestProcessedStateRoot
     ) where
 
+import Data.Maybe
 import qualified Database.Esqueleto as E
+import qualified Database.Persist.Sql as SQL
 
 import Blockchain.Data.DataDefs
 import qualified Blockchain.Database.MerklePatricia as MP
@@ -13,20 +15,18 @@ import Blockchain.DB.SQLDB
 getBestProcessedStateRoot::HasSQLDB m=>
                            m (MP.SHAPtr, Integer)
 getBestProcessedStateRoot = do
-  ret <-
-      sqlQuery $
-      E.select $ 
-       E.from $ \kv -> do
-         E.where_ (kv E.^. ExtraKey E.==. E.val "bestBlockNumber")
-         return $ kv E.^. ExtraValue
+  ret <- sqlQuery $ 
+         E.select $ E.from $ \kv -> do
+             E.where_ (kv E.^. ExtraKey E.==. E.val "bestBlockNumber")
+             return $ kv E.^. ExtraValue
 
   case ret of
-    [x] -> return undefined -- $ read . E.unValue . head $ x
-    [] -> error "Ethereum DBs are blank, you need to set them up by running 'ethereum-setup'"
-    _ -> error "getBestBlockHash can't handle a tie yet, yet that is what we have."
+    [x] -> return $ read $ E.unValue x
+    _ -> error "Ethereum DBs are blank, you need to set them up by running 'ethereum-setup'"
 
 
 putBestProcessedStateRoot::HasSQLDB m=>
                            MP.SHAPtr->Integer->m ()
-putBestProcessedStateRoot = undefined
-
+putBestProcessedStateRoot stateRoot bestNumber = do
+  sqlQuery $ SQL.upsert (Extra "bestBlockNumber" $ show (stateRoot, bestNumber)) []
+  return ()
