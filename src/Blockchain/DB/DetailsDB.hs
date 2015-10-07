2f3e6e25af42a2e6aa0720902f61a0dd53bf59fa
user <user@blockapps.net>
2015-10-07 02:12:55 -0400

    Removing unneeded function.

--- src/Blockchain/DB/DetailsDB.hs
+++ src/Blockchain/DB/DetailsDB.hs
@@ -2,8 +2,7 @@
 module Blockchain.DB.DetailsDB (
   getBestBlockHash,
   getGenesisBlockHash,
-  getBestBlock,
-  getBestProcessedBlock
+  getBestBlock
   ) where
 
 import Data.Maybe
@@ -28,20 +27,6 @@ getBestBlockHash = do
     [] -> error "Ethereum DBs are blank, you need to set them up by running 'ethereum-setup'"
     _ -> error "getBestBlockHash can't handle a tie yet, yet that is what we have."
 
-getBestProcessedBlockHash::HasSQLDB m=>
-                           m SHA
-getBestProcessedBlockHash = do
-  ret <- sqlQuery $
-         E.select $ E.from $ \(bd `E.InnerJoin` unprocessed) -> do
-           E.on (bd E.^. BlockDataRefBlockId E.==. unprocessed E.^. UnprocessedBlockId)
-           E.limit 1
-           E.orderBy [E.desc (bd E.^. BlockDataRefTotalDifficulty)]
-           return $ bd E.^. BlockDataRefHash
-  case ret of
-    [x] -> return $ E.unValue x
-    [] -> error "Ethereum DBs are blank, you need to set them up by running 'ethereum-setup'"
-    _ -> error "getBestBlockHash can't handle a tie yet, yet that is what we have."
-
 getGenesisBlockHash::HasSQLDB m=>
                      m SHA
 getGenesisBlockHash = do
@@ -58,13 +43,6 @@ getBestBlock::HasSQLDB m=>
               m Block
 getBestBlock = do
   bestBlockHash <- getBestBlockHash
-  bestBlock <- getBlock bestBlockHash
-  return $ fromMaybe (error $ "Missing block in database: " ++ format (bestBlockHash)) bestBlock
-
-getBestProcessedBlock::HasSQLDB m=>
-                       m Block
-getBestProcessedBlock = do
-  bestBlockHash <- getBestProcessedBlockHash
   bestBlock <- getBlock bestBlockHash
   return $ fromMaybe (error $ "Missing block in database: " ++ format (bestBlockHash)) bestBlock
 
